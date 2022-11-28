bcc44715df35100f632c5d76bae0af39ec811a7d
Maksim Begun <maksbegun@icloud.com>
2022-11-28 22:30:47 +0300

    initial release

---
+++ QR Scanner App/QR Scanner App/Controllers/LastScansViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class LastScansViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ QR Scanner App/QR Scanner App/Supporting/CoreData/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  QR Scanner App
+//
+//  Created by Maksim Begun on 28.11.2022.
+//
+
+import Foundation
